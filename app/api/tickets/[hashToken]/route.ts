749901f535d966aa7d8f537a88a895b3feeab48a
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2025-11-30 10:18:29 +0100

    include map ds for redundant ticket entries

--- app/api/tickets/[hashToken]/route.ts
+++ app/api/tickets/[hashToken]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/mongodb";
-import { cookies } from "next/headers";
+import {NextResponse} from "next/server";
+import {connectDB} from "@/lib/mongodb";
+import {cookies} from "next/headers";
+import Event from "@/models/Event";
 import Ticket from "@/models/Ticket";
 import User from "@/models/User";
-import Event from "@/models/Event";
 import jwt, {JwtPayload} from "jsonwebtoken";
 
 
@@ -11,10 +11,9 @@ type Params = {
     params: Promise<{ hashToken: string }>;
 };
 
-export async function GET(req: Request, { params }: Params) {
+export async function GET(req: Request, {params}: Params) {
     try {
         await connectDB();
-
         const token = (await cookies()).get("token")?.value;
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET as string)
@@ -23,46 +22,59 @@ export async function GET(req: Request, { params }: Params) {
         }
 
         const userId = (decoded as JwtPayload).id;
-        // const event = await Event.findById(oar);
-        // console.log({  });
-        
+
         if (!token) {
             return NextResponse.json(
-                { error: "Unauthorized: No token provided" },
-                { status: 401 }
+                {error: "Unauthorized: No token provided"},
+                {status: 401}
             );
         }
 
         // ✅ Await v directly — not props.context.v
-        const { hashToken: eventId } = await params;
-        
-        const event = await Event.findById(eventId);
-        const tickets = await Ticket.find({ createdBy: userId });
+        const {hashToken: eventId} = await params;
+
+        const event = await Event.findById(eventId)
+        const tickets = await Ticket.find({ createdBy: userId }).populate("event");
+        // const tickets = await Ticket.find({createdBy: userId});
+        const ticketList = new Map<string, { event: any, tickets: any[] }>()
+
+        for (const ticket of tickets) {
+            const _eventId = ticket.event._id.toString();
+
+            if (_eventId !== eventId) {
+                continue;
+            }
+            
+            if (!ticketList.has(_eventId)) {
+                ticketList.set(_eventId, {
+                    event: ticket.event,
+                    tickets: []
+                })
+            }
+            ticketList.get(_eventId)!.tickets.push(ticket)
+        }
         if (!tickets) {
             return NextResponse.json(
-                { error: "Could not find event for this ticket" },
-                { status: 404 }
+                {error: "Could not find event for this ticket"},
+                {status: 404}
             );
         }
-
+        const matchedTicket = ticketList.get(eventId);
+        console.log({ticketList, matchedTicket});
         const response = {
             event,
-            tickets
+            tickets: matchedTicket,
         }
-        console.log("Ticket number:", {event, tickets});
-        // const response = {
-        //     tickets,
-        // }
-        //
+        
         return NextResponse.json(
-            { message: "Ticket found", response },
-            { status: 200 }
+            {message: "Ticket found", response},
+            {status: 200}
         );
     } catch (error) {
         console.error("Error:", error);
         return NextResponse.json(
-            { error: "Internal Server Error" },
-            { status: 500 }
+            {error: "Internal Server Error"},
+            {status: 500}
         );
     }
 }
