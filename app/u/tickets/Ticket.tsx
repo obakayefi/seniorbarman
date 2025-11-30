aa50b00559b0a87384af28afdef2d11f8d2a0d0e
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2025-11-30 10:18:45 +0100

    include extra information on ticket comp.

--- app/u/tickets/Ticket.tsx
+++ app/u/tickets/Ticket.tsx
@@ -1,27 +1,35 @@
-import { TicketStandIcon } from '@/components/icons'
-import { TicketIcon } from 'lucide-react'
+import {TicketStandIcon} from '@/components/icons'
+import {TicketIcon} from 'lucide-react'
 import Image from 'next/image'
 import {useQRCode} from "next-qrcode";
 import {extractTicketStatus} from "@/lib/utils";
 
-const Ticket = ({ ticket }: { ticket: any }) => {
-    const { Image } = useQRCode();
+const Ticket = ({ticket}: { ticket: any }) => {
+    const {Image} = useQRCode();
+
+    if (!ticket) return null
 
     const status_checkedIn = extractTicketStatus(ticket.checkInLogs) === "Checked In"
     const status_checkedOut = extractTicketStatus(ticket.checkInLogs) === "Checked Out"
     const status_notCheckedIn = extractTicketStatus(ticket.checkInLogs) === "Not Checked In"
 
-    // console.log({clientticket: ticket})
-    
+    console.log({clientTicket: ticket, event: ticket.event, checkInLogs: status_checkedIn})
+
     const statusBgColor = status_checkedIn ? "bg-green-200 text-green-700" : status_checkedOut ? "bg-red-200 text-red-700 " : status_notCheckedIn ? "bg-slate-800 text-slate-200" : null
 
     return (
-        <div className='flex flex-col md:flex-row items-center hover:bg-gray-100/50 duration-200 border-2 border-gray-100 p-2 rounded gap-4'>
+        <div
+            className='flex flex-col md:flex-row items-center hover:bg-gray-100/50 duration-200 border-2 border-gray-100 p-2 rounded gap-4'>
             <section className='flex gap-2 flex-col-reverse md:flex-col overflow-hidden'>
                 <div>
                     <div className='bg-[#F5F5F5] flex justify-center items-center gap-2 px-4 py-2'>
-                        <span><TicketStandIcon size={24} /></span>
+                        <span><TicketStandIcon size={24}/></span>
                         <span>{ticket.stand}</span>
+                    </div>
+                    <div className='text-slate-700 text-center mb-3 mt-2'>
+                        <h2 className="text-sm">{ticket.event.homeTeam}</h2>
+                        <h2 className="text-sm">vs</h2>
+                        <h2 className="text-sm">{ticket.event.awayTeam}</h2>
                     </div>
                 </div>
 
@@ -43,7 +51,7 @@ const Ticket = ({ ticket }: { ticket: any }) => {
                 />
                 <p className={`bg-[#3C3C3C] text-xs uppercase text-center py-1 text-[#9D9D9D] rounded ${statusBgColor}`}>{extractTicketStatus(ticket.checkInLogs)}</p>
             </section>
-            
+
 
         </div>
     )
