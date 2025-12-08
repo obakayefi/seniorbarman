f52c8c78cd0d550cd9a31e6af6c3d05571d89dc2
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2025-12-08 15:54:27 +0100

    remove dead code

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -22,11 +22,7 @@ export async function GET(req: Request) {
                 eventStart.setHours(Number(hours));
                 eventStart.setMinutes(Number(minutes));
                 eventStart.setSeconds(0);
-                // console.log({eventStart, cutoff})
-                // return eventStart >= cutoff;
-                // Add 24 hours
                 const eventEnd = new Date(eventStart.getTime() + 24 * 60 * 60 * 1000);
-                // Keep if current time is before eventEnd
                 return eventEnd >= cutoff;
             })
         }
